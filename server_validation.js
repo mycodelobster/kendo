afb80c0145ca664743e70012c22e8ecc604e4913
mycodelobster <mycodelobster@outlook.com>
2015-03-21 00:16:21 +0700

    Update server_validation.js

--- server_validation.js
+++ server_validation.js
@@ -1,9 +1,13 @@
 
+				<span kendo-notification="formError" k-append-to="'.k-window'" k-animation='false'></span>
+				
 				serverFiltering: true,
-				error: function error(e) {        
-					if (e.errors) {
+				error: function error(arg) { 
+					if (arg.errors) {
 						$scope.grid.one("dataBinding", function (e) {   
 							e.preventDefault();   
 						});
+						$scope.formError.show(arg.errors, "error");
+						console.log(arg.errors);
 					}
 				},
